d500511c8049c20f1c3682ac553adb0b7b178942
Jacob Larson <hockeybobber@comcast.net>
2024-11-05 15:37:20 -0600

    deleted if statment that was used with cypress

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,10 +2,6 @@ const pg = require('pg')
 
 let databaseName = 'react-gallery'
 
-if (process.env.NODE_ENV === 'test') {
-  databaseName = 'prime_testing'
-}
-
 const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
