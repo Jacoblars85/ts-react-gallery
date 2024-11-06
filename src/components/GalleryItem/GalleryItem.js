9e97f75bc848f8a01c4896fa0b5b6406f00249a7
Jacob Larson <hockeybobber@comcast.net>
2024-11-06 17:01:22 -0600

    added the converted ts file for the item

---
+++ src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function GalleryItem() {
+    return (<div>
+        <img src="images/goat_small.jpg"/>
+      </div>);
+}
+exports.default = GalleryItem;
