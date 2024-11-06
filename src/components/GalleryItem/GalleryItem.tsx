b769d175558c82dab82d3f2101a7ad6f0f174cad
Jacob Larson <hockeybobber@comcast.net>
2024-11-06 16:42:12 -0600

    cleaned up it and set up for the code

--- src/components/GalleryItem/GalleryItem.tsx
+++ src/components/GalleryItem/GalleryItem.tsx
@@ -1,14 +1,11 @@
+
+
+
 function GalleryItem() {
-    
+
     return (
       <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
-
-        <p>The gallery goes here!</p>
         <img src="images/goat_small.jpg"/>
-        <img src="images/goat_stache.png"/>
       </div>
     );
 }
