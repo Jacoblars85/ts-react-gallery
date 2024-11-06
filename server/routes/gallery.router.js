953107ae57cb092658f3b2938f97d624f68fab44
Jacob Larson <hockeybobber@comcast.net>
2024-11-06 16:34:11 -0600

    created the converted ts file

---
+++ server/routes/gallery.router.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+// PUT /gallery/like/:id
+router.put('/like/:id', function (req, res) {
+    // code here
+});
+// GET /gallery
+router.get('/', function (req, res) {
+    // code here
+});
+module.exports = router;
