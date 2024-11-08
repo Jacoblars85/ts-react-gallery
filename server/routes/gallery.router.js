1599cde589bf5eaa0a0c3bf2290fa41fee05e6f8
Jacob Larson <hockeybobber@comcast.net>
2024-11-07 19:35:06 -0600

    added get route

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
-// PUT /gallery/like/:id
-router.put('/like/:id', function (req, res) {
-    // code here
-});
+var pool = require('../modules/pool');
 // GET /gallery
 router.get('/', function (req, res) {
+    var sqlQueryText = "\n  SELECT * FROM \"gallery\"\n  ORDER BY \"id\";\n";
+    pool.query(sqlQueryText)
+        .then(function (result) {
+        console.log('result', result.rows);
+        res.send(result.rows);
+    }).catch(function (err) {
+        res.sendStatus(500);
+        console.log('error in server get route', err);
+    });
+});
+// PUT /gallery/like/:id
+router.put('/like/:id', function (req, res) {
     // code here
 });
 module.exports = router;
