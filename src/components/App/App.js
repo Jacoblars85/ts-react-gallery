71e034e0befa02e2dd85fb3acc9b643aaee59b6d
Jacob Larson <hockeybobber@comcast.net>
2024-11-08 22:35:01 -0600

    has the new code for the logs and code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var GalleyList_1 = require("../GalleryList/GalleyList");
+var react_1 = require("react");
+var react_2 = require("react");
+var axios_1 = require("axios");
+require("./App.css");
 function App() {
+    var _a = (0, react_2.useState)([]), galleryList = _a[0], setGalleryList = _a[1];
+    (0, react_2.useEffect)(function () {
+        getGalleryList();
+    }, []);
+    var getGalleryList = function () {
+        (0, axios_1.default)({
+            method: 'GET',
+            url: '/gallery'
+        })
+            .then(function (response) {
+            setGalleryList(response.data);
+        })
+            .catch(function (error) {
+            console.log('getGalleryList fail:', error);
+        });
+    };
+    console.log('galleryList in app', galleryList);
     return (<div>
         <header>
           <h1>React Gallery</h1>
@@ -11,7 +32,7 @@ function App() {
         <img src="images/goat_small.jpg"/>
         <img src="images/goat_stache.png"/>
 
-        <GalleyList_1.default />
+        <GalleyList_1.default galleryList={galleryList} getGalleryList={getGalleryList}/>
       </div>);
 }
 exports.default = App;
