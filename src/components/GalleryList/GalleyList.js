b6c661e1fe1a0b38e2d0c60a38e7c194c1cc0337
Jacob Larson <hockeybobber@comcast.net>
2024-11-08 22:35:38 -0600

    has the inserted code and logs

--- src/components/GalleryList/GalleyList.js
+++ src/components/GalleryList/GalleyList.js
@@ -1,16 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var GalleryItem_1 = require("../GalleryItem/GalleryItem");
-function GalleryList() {
+function GalleryList(_a) {
+    var galleryList = _a.galleryList;
+    console.log('galleryList', galleryList);
     return (<div>
       <header>
         <h1>Gallery list</h1>
       </header>
 
-      {gallery.map(function (pic) {
+      {galleryList.map(function (galleryItem) {
             return (<div key={pic.id}>
 
-              <GalleryItem_1.default pic={pic}/>
+              <GalleryItem_1.default galleryItem={galleryItem}/>
                 
             </div>);
         })}
