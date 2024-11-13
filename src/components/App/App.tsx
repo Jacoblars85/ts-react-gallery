df7a1f11e99911a396c776ec803db8010e3d7b51
Jacob Larson <hockeybobber@comcast.net>
2024-11-12 22:45:10 -0600

    deleted the pre done pics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -35,10 +35,6 @@ function App() {
           <h1>React Gallery</h1>
         </header>
 
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
-        <img src="images/goat_stache.png"/>
-
         <GalleryList galleryList={galleryList} getGalleryList={getGalleryList} />
       </div>
     );
