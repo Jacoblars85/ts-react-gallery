57f0ecfdd187b89431295e2f373a98d4b0e11ca3
Jacob Larson <hockeybobber@comcast.net>
2024-11-14 19:58:46 -0600

    formatted doc

--- src/components/GalleryItem/GalleryItem.tsx
+++ src/components/GalleryItem/GalleryItem.tsx
@@ -1,40 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function GalleryItem({ galleryItem }) {
+  const [isPicture, setIsPicture] = useState(false);
 
-    const [isPicture, setIsPicture] = useState(false)
+  const togglePicture = () => {
+    setIsPicture(!isPicture);
+  };
 
-    const togglePicture = () => {
-        setIsPicture(!isPicture)
-      }
+  const displayText = () => {
+    if (isPicture) {
+      return <>{galleryItem.description}</>;
+    } else {
+      return (
+        <>
+          <img src={galleryItem.url} />
+        </>
+      );
+    }
+  };
 
-      const displayText = () => {
-        if (isPicture) {
-          return (
-              <>
-{galleryItem.description}
-              </>
-          )
-        } else {
-          return (
-              <>
- <img src={galleryItem.url}/>
-              </>
-          )
-        }
-      }
+  return (
+    <div>
+      <div onClick={togglePicture}>{displayText()}</div>
 
-    return (
-      <div>
-
-       <div onClick={togglePicture}>
-        {displayText()}
-       </div>
-       
-        {galleryItem.title}
-
-      </div>
-    );
+      {galleryItem.title}
+    </div>
+  );
 }
 
 export default GalleryItem;
