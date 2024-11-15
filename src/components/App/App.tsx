1281a5e3c2d7f5ef6e50cd7057f913280dee68e1
Jacob Larson <hockeybobber@comcast.net>
2024-11-14 20:02:00 -0600

    formatted doc

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,42 +1,40 @@
 import GalleryList from "../GalleryList/GalleyList";
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-
   const [galleryList, setGalleryList] = useState([]);
 
   useEffect(() => {
-    getGalleryList()
-  }, [])
+    getGalleryList();
+  }, []);
 
   const getGalleryList = () => {
     axios({
-      method: 'GET',
-      url: '/api/gallery'
+      method: "GET",
+      url: "/api/gallery",
     })
       .then((response) => {
-        setGalleryList(response.data)
+        setGalleryList(response.data);
       })
       .catch((error) => {
-        console.log('getGalleryList fail:', error);
-      })
-  }
+        console.log("getGalleryList fail:", error);
+      });
+  };
 
-  console.log('galleryList in app', galleryList);
+  console.log("galleryList in app", galleryList);
 
-    return (
-      <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
+  return (
+    <div>
+      <header>
+        <h1>React Gallery</h1>
+      </header>
 
-        <GalleryList galleryList={galleryList} getGalleryList={getGalleryList} />
-      </div>
-    );
+      <GalleryList galleryList={galleryList} getGalleryList={getGalleryList} />
+    </div>
+  );
 }
 
 export default App;
