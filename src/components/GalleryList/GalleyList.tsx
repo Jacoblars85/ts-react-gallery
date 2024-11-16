9c0a25d0d1fada41383e7a2c3ef0b2c4ce4cb6d3
Jacob Larson <hockeybobber@comcast.net>
2024-11-15 20:44:44 -0600

    formatted doc

--- src/components/GalleryList/GalleyList.tsx
+++ src/components/GalleryList/GalleyList.tsx
@@ -3,16 +3,17 @@ import GalleryItem from "../GalleryItem/GalleryItem";
 function GalleryList({ galleryList, getGalleryList }) {
   return (
     <div>
-
-
-<div className="galleryListBox">
-      {galleryList.map((galleryItem) => {
-        return (
-          <div key={galleryItem.id} className="galleryItemBox">
-            <GalleryItem galleryItem={galleryItem} getGalleryList={getGalleryList} />
-          </div>
-        );
-      })}
+      <div className="galleryListBox">
+        {galleryList.map((galleryItem) => {
+          return (
+            <div key={galleryItem.id} className="galleryItemBox">
+              <GalleryItem
+                galleryItem={galleryItem}
+                getGalleryList={getGalleryList}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
